5b684eeec31d5d3eea43f0d05d282cc602627584
David-The-Programmer <42868389+David-The-Programmer@users.noreply.github.com>
2023-12-19 09:56:50 +0000

    Complete Day 2 Part 2

---
+++ 2023/d2/pt2/solution.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Open(path + "/d2/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	idSum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		s := strings.Split(line, ":")[1]
+		if err != nil {
+			log.Fatal(err)
+		}
+		picks := strings.Split(s, ";")
+		maxNumRed := 0
+		maxNumGreen := 0
+		maxNumBlue := 0
+		for _, pick := range picks {
+			colourCounts := strings.Split(pick, ", ")
+			for _, colourCount := range colourCounts {
+				colourCount = strings.TrimSpace(colourCount)
+				s := strings.Split(colourCount, " ")
+				count, err := strconv.Atoi(s[0])
+				if err != nil {
+					log.Fatal(err)
+				}
+				colour := s[1]
+				if colour == "red" {
+					if maxNumRed < count {
+						maxNumRed = count
+					}
+				} else if colour == "blue" {
+					if maxNumBlue < count {
+						maxNumBlue = count
+					}
+				} else {
+					if maxNumGreen < count {
+						maxNumGreen = count
+					}
+				}
+			}
+		}
+		idSum += maxNumRed * maxNumBlue * maxNumGreen
+	}
+	fmt.Println(idSum)
+}
