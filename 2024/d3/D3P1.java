53aa3b5e98708ce9fa1be948b6f77483b4bc95a7
David-The-Programmer <42868389+David-The-Programmer@users.noreply.github.com.>
2024-12-03 18:35:33 +0800

    puzzle: AOC 2024 Day 3 complete

---
+++ 2024/d3/D3P1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Arrays;
+
+public class D3P1 {
+
+  public static void main(String[] args) {
+    try {
+      Scanner scanner = new Scanner(new File("./input.txt"));
+      int prodSum = 0;
+      Pattern pattern = Pattern.compile("mul\\([0-9]+,[0-9]+\\)");
+      while(scanner.hasNextLine()) {
+        String s = scanner.nextLine();
+        Matcher matcher = pattern.matcher(s);
+        while(matcher.find()) {
+          String subStr = s.substring(matcher.start(), matcher.end()).trim();
+          subStr = subStr.substring(4, subStr.length() - 1);
+          prodSum += Arrays
+          .stream(subStr.split(","))
+          .map(x -> Integer.parseInt(x))
+          .reduce(1, (acc, x) -> acc * x);
+        }
+      }
+      System.out.println("Total sum of products is " + prodSum);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
