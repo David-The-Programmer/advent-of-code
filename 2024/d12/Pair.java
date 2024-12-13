2d98fe4b47170f95dbb17f29e3662560e8610beb
David-The-Programmer <42868389+David-The-Programmer@users.noreply.github.com.>
2024-12-13 14:47:34 +0800

    puzzle: AOC 2024 Day 12 Complete

---
+++ 2024/d12/Pair.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Pair<T, S> {
+  private T first;
+  private S second;
+
+  public Pair(T first, S second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public T first() {
+    return this.first;
+  }
+
+  public S second() {
+    return this.second;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + this.first + "," + this.second + ")";
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if(!(obj instanceof Pair)) {
+      return false;
+    }
+    Pair<?, ?> p = (Pair<?, ?>) obj;
+    return this.first.equals(p.first()) && this.second.equals(p.second());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(first, second);
+  }
+}
